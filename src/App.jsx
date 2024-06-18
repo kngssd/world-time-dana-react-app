c4a763365c6c6d6480e7d761bf7249e8c2b2040e
2405-007-dk <danakengessova@gmail.com>
2024-06-18 16:33:16 +0100

    final changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react"
+import "./App.css"
+import moment from "moment-timezone"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const London = moment().tz('Europe/London').format('HH:mm')
+  const Almaty = moment().tz('Asia/Almaty').format('HH:mm')
+  const Paris = moment().tz('Europe/Paris').format('HH:mm')
+  const Edinburgh = moment().tz('Europe/Edinburgh').format('HH:mm')
+
+  const date = new Date()
+  const hours = date.getHours() % 12
+  let timeOfDay
+
+  if (hours < 12) {
+    timeOfDay = "morning"
+  } else if (hours >= 12 && hours < 19) {
+    timeOfDay = "afternoon"
+  } else {
+    timeOfDay = "night"
+  }
 
   return (
     <>
+    <div>
+      <h1>Good {timeOfDay}! It is Dana. </h1>
+      </div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>It is currently {London} in London. </h1>
+      <h1>It is currently {Almaty} in Almaty. </h1>
+      <h1>It is currently {Paris} in Paris. </h1>
+      <h1>It is currently {Edinburgh} in Edinburgh. </h1>
+    </div>
     </>
   )
 }
